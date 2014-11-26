856f0f67ae1e9d9791fe79472cec755935235d94
Sukjoon Kim <sjoonk@gmail.com>
2014-11-26 10:30:21 +0900

    update entry template

--- public/js/main.js
+++ public/js/main.js
@@ -60,9 +60,8 @@ var App = (function() {
         initialize: function() {
             this.listenTo(this.model, 'destroy', this.remove); // call view.remove()
         },
-        template: _.template('{{title}} \
-            <a href="#" class="delete"><span class="genericon genericon-trash"></span></a> \
-        '),
+        template: _.template('{{title}}' +
+            '<a href="#" class="delete"><span class="genericon genericon-trash"></span></a>'),
         render: function() {
             $(this.el).html(this.template(this.model.attributes));
             return this;
